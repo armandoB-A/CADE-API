4e745bae1c18d66df32e8f88e0dae6d10d240895
armandoB-A <blasarce@gnail.com>
2023-01-16 22:25:04 -0600

    cross origin alumno

--- src/main/java/com/bcode/cade/controllers/AlumnoController.java
+++ src/main/java/com/bcode/cade/controllers/AlumnoController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("alumno")
 public class AlumnoController {
